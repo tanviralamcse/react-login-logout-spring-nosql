c9bdd129cea89fa19a49de715bf52ef25a7c00b3
tanviralamcse <tanviralam.cse@gmail.com>
2022-07-27 05:46:48 +0200

    register page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <main className="App">
+        <Register/>
+    </main>
+  );
+}
+
+export default App;
